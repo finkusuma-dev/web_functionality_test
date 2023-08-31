f6d48e480b17274d2d6c2d0f2fe7002ade3794d8
Arifin Kusuma <finkusuma.dev@gmail.com>
2023-08-31 21:39:58 +0700

    changed view timeline. New remote Js library fix: refresh page broke the animation

--- animations/view_timeline_js.js
+++ animations/view_timeline_js.js
@@ -1,52 +1,52 @@
-// import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
-import './scroll-timeline.js';
+import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
+// import './scroll-timeline.js';
 
 /// animated one box
-const animatedBox = document.querySelector('#box-animated');
+// const animatedBox = document.querySelector('#box-animated');
 
-animatedBox.animate(
-  {
-    backgroundColor: ['red', 'darkred'],
-    // width: ['0', '100%'],
-    transform: ['scaleX(0)', 'scaleX(1)'],
-  },
-  {
-    // duration: 1,
-    fill: 'both',
-    timeline: new ViewTimeline({
-      subject: animatedBox,
-      source: document.documentElement,
-    }),
-    rangeStart: { rangeName: 'contain', offset: CSS.percent(20) },
-    rangeEnd: { rangeName: 'contain', offset: CSS.percent(80) },
-    // rangeStart: { rangeName: 'contain', offset: CSS.percent(0) },
-    // rangeEnd: { rangeName: 'contain', offset: CSS.percent(50) },
-  }
-);
+// animatedBox.animate(
+//   {
+//     backgroundColor: ['red', 'darkred'],
+//     // width: ['0', '100%'],
+//     transform: ['scaleX(0)', 'scaleX(1)'],
+//   },
+//   {
+//     // duration: 1,
+//     fill: 'both',
+//     timeline: new ViewTimeline({
+//       subject: animatedBox,
+//       source: document.documentElement,
+//     }),
+//     rangeStart: { rangeName: 'contain', offset: CSS.percent(20) },
+//     rangeEnd: { rangeName: 'contain', offset: CSS.percent(80) },
+//     // rangeStart: { rangeName: 'contain', offset: CSS.percent(0) },
+//     // rangeEnd: { rangeName: 'contain', offset: CSS.percent(50) },
+//   }
+// );
 
 /// animated boxes with specified class
-// const animatedBoxes = document.querySelectorAll('.box-animated');
+const animatedBoxes = document.querySelectorAll('.box-animated');
 
-// animatedBoxes.forEach((animatedBox) => {
-//   animatedBox.animate(
-//     {
-//       backgroundColor: ['red', 'darkred'],
-//       width: ['0', '100%'],
-//       // transform: ['scaleX(0)', 'scaleX(1)'],
-//     },
-//     {
-//       // duration: 1,
-//       fill: 'both',
-//       timeline: new ViewTimeline({
-//         subject: animatedBox,
-//       }),
-//       rangeStart: { rangeName: 'contain', offset: CSS.percent(20) },
-//       rangeEnd: { rangeName: 'contain', offset: CSS.percent(100) },
-//       // rangeStart: { rangeName: 'contain', offset: CSS.percent(0) },
-//       // rangeEnd: { rangeName: 'contain', offset: CSS.percent(50) },
-//     }
-//   );
-// });
+animatedBoxes.forEach((animatedBox) => {
+  animatedBox.animate(
+    {
+      backgroundColor: ['red', 'darkred'],
+      width: ['0', '100%'],
+      // transform: ['scaleX(0)', 'scaleX(1)'],
+    },
+    {
+      // duration: 1,
+      fill: 'both',
+      timeline: new ViewTimeline({
+        subject: animatedBox,
+      }),
+      rangeStart: { rangeName: 'contain', offset: CSS.percent(20) },
+      rangeEnd: { rangeName: 'contain', offset: CSS.percent(100) },
+      // rangeStart: { rangeName: 'contain', offset: CSS.percent(0) },
+      // rangeEnd: { rangeName: 'contain', offset: CSS.percent(50) },
+    }
+  );
+});
 
 /// old and not worked implementation
 
